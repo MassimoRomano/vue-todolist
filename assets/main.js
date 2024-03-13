0ab76c4a7f03edc9aec044468ceed7510a9ea2dc
MassimoRomano <romano93m@gmail.com>
2024-03-13 17:08:30 +0100

    add input,button and start js

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,27 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-        
+            newTask: [
+                {
+                    text: '',
+                    done: false
+                },
+            ],
+
+            tasks: [
+                {
+                    text: 'ciao',
+                    done: true
+                },
+                {
+                    text: 'arrivederci',
+                    done: false
+                },
+                {
+                    text: 'buonanotte',
+                    done: true
+                }
+            ]
         }
     }
 })
