c58e709360c85ff708a1b88ec327f82d1f6ba91f
MassimoRomano <romano93m@gmail.com>
2024-03-13 18:35:20 +0100

    add line and remove

--- assets/main.js
+++ assets/main.js
@@ -34,6 +34,13 @@ createApp({
             this.tasks.unshift(this.newTask)
             this.newTask = ''
         },
-        
+        /* funzione per eliminare le task dalla dom */
+        removeTask(index) {
+            this.tasks.splice(index, 1)
+        },
+        /* funzione per aggiunger una linea sulla task nel caso ci clicco */
+        lineCheck(index) {
+            this.tasks[index].done = !this.tasks[index].done
+        }
     }
 }).mount('#app')
